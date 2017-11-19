96214565cf7dda88fe98478c1a2b95aec015ab6d
Giovanni Pardo <gpardo@fb.com>
2017-11-19 16:15:41 -0600

    no message

---
+++ assets/javascript/game.js
@@ -0,0 +1,39 @@
+document.ready(function(){
+
+
+var wins = 0;
+var losses = 0;
+var button-yellow, button-blue, button-red, button-green;
+var randomComputerValue;
+var userTallyScore;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
